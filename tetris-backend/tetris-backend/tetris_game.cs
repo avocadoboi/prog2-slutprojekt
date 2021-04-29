f1a872f9b96a4c089382aa0339c430b9b1337acc
Björn Sundin <bjornsundin02@gmail.com>
2021-04-29 10:25:45 +0200

    Create tetris backend solution with class library and testing projects

---
+++ tetris-backend/tetris-backend/tetris_game.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace tetris_backend {
+    
+public enum TetrisInput {
+    Left,
+    Right,
+    RotateLeft,
+    RotateRight,
+    SwapTetromino,
+    SkipFall,
+    Restart
+}
+
+public enum CellType {
+    Empty, I, J, L, O, S, T, Z
+}
+
+}
