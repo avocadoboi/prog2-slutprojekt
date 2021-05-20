edeeeeebfa8f4651451211c1904c07a27a0756e5
Björn Sundin <bjornsundin02@gmail.com>
2021-05-20 09:00:22 +0200

    Normalize indentation in test files

--- tetris-backend/tetris-backend-tests/board.cs
+++ tetris-backend/tetris-backend-tests/board.cs
@@ -4,57 +4,57 @@ using TetrisBackend;
 
 namespace TetrisBackendTests
 {
-    public class BoardTests
-    {
-        [Fact]
-        public void BoardDrawing()
-        {
-            var game = new TetrisGame(new Vec2i(10, 20), new PlayerScoreFile("helo.txt"));
+	public class BoardTests
+	{
+		[Fact]
+		public void BoardDrawing()
+		{
+			var game = new TetrisGame(new Vec2i(10, 20), new PlayerScoreFile("helo.txt"));
 
-            var board = new TetrisBoard(new Vec2i(6, 12));
-            board.Draw(new Tetromino(TetrisCell.L, new int[,] {
-                {0, 0, 1},
-                {1, 1, 1},
-                {0, 0, 0},
-            }), new Vec2i(2, 5));
-            board.Draw(new Tetromino(TetrisCell.T, new int[,] {
-                {0, 1, 0},
-                {1, 1, 1},
-                {0, 0, 0},
-            }), new Vec2i(1, 4));
+			var board = new TetrisBoard(new Vec2i(6, 12));
+			board.Draw(new Tetromino(TetrisCell.L, new int[,] {
+				{0, 0, 1},
+				{1, 1, 1},
+				{0, 0, 0},
+			}), new Vec2i(2, 5));
+			board.Draw(new Tetromino(TetrisCell.T, new int[,] {
+				{0, 1, 0},
+				{1, 1, 1},
+				{0, 0, 0},
+			}), new Vec2i(1, 4));
 
-            // I'm sorry
-            var expected_board = new TetrisCell[12][] {
-                new TetrisCell[6]{0, 0,            0,            0,            0,            0},
-                new TetrisCell[6]{0, 0,            0,            0,            0,            0},
-                new TetrisCell[6]{0, 0,            0,            0,            0,            0},
-                new TetrisCell[6]{0, 0,            0,            0,            0,            0},
-                new TetrisCell[6]{0, 0,            TetrisCell.T, 0,            0,            0},
-                new TetrisCell[6]{0, TetrisCell.T, TetrisCell.T, TetrisCell.T, TetrisCell.L, 0},
-                new TetrisCell[6]{0, 0,            TetrisCell.L, TetrisCell.L, TetrisCell.L, 0},
-                new TetrisCell[6]{0, 0,            0,            0,            0,            0},
-                new TetrisCell[6]{0, 0,            0,            0,            0,            0},
-                new TetrisCell[6]{0, 0,            0,            0,            0,            0},
-                new TetrisCell[6]{0, 0,            0,            0,            0,            0},
-                new TetrisCell[6]{0, 0,            0,            0,            0,            0},
+			// I'm sorry
+			var expected_board = new TetrisCell[12][] {
+				new TetrisCell[6]{0, 0,            0,            0,            0,            0},
+				new TetrisCell[6]{0, 0,            0,            0,            0,            0},
+				new TetrisCell[6]{0, 0,            0,            0,            0,            0},
+				new TetrisCell[6]{0, 0,            0,            0,            0,            0},
+				new TetrisCell[6]{0, 0,            TetrisCell.T, 0,            0,            0},
+				new TetrisCell[6]{0, TetrisCell.T, TetrisCell.T, TetrisCell.T, TetrisCell.L, 0},
+				new TetrisCell[6]{0, 0,            TetrisCell.L, TetrisCell.L, TetrisCell.L, 0},
+				new TetrisCell[6]{0, 0,            0,            0,            0,            0},
+				new TetrisCell[6]{0, 0,            0,            0,            0,            0},
+				new TetrisCell[6]{0, 0,            0,            0,            0,            0},
+				new TetrisCell[6]{0, 0,            0,            0,            0,            0},
+				new TetrisCell[6]{0, 0,            0,            0,            0,            0},
 			};
-            Assert.Equal(expected_board, board.Cells);
-        }
-        [Fact]
-        public void ClearRows()
-        {
-            var board = new TetrisBoard(new Vec2i(3, 10));
+			Assert.Equal(expected_board, board.Cells);
+		}
+		[Fact]
+		public void ClearRows()
+		{
+			var board = new TetrisBoard(new Vec2i(3, 10));
 			board.Cells = new TetrisCell[10][] {
-                new TetrisCell[3]{TetrisCell.Z, TetrisCell.Z, 0           },
-                new TetrisCell[3]{TetrisCell.J, TetrisCell.Z, TetrisCell.Z},
-                new TetrisCell[3]{TetrisCell.J, TetrisCell.J, TetrisCell.J},
-                new TetrisCell[3]{TetrisCell.L, TetrisCell.L, TetrisCell.L},
-                new TetrisCell[3]{TetrisCell.L, 0,            TetrisCell.Z},
-                new TetrisCell[3]{0,            TetrisCell.Z, TetrisCell.Z},
-                new TetrisCell[3]{TetrisCell.J, TetrisCell.Z, 0           },
-                new TetrisCell[3]{TetrisCell.J, TetrisCell.J, TetrisCell.J},
-                new TetrisCell[3]{TetrisCell.T, TetrisCell.T, TetrisCell.T},
-                new TetrisCell[3]{0,            TetrisCell.T, 0           },
+				new TetrisCell[3]{TetrisCell.Z, TetrisCell.Z, 0           },
+				new TetrisCell[3]{TetrisCell.J, TetrisCell.Z, TetrisCell.Z},
+				new TetrisCell[3]{TetrisCell.J, TetrisCell.J, TetrisCell.J},
+				new TetrisCell[3]{TetrisCell.L, TetrisCell.L, TetrisCell.L},
+				new TetrisCell[3]{TetrisCell.L, 0,            TetrisCell.Z},
+				new TetrisCell[3]{0,            TetrisCell.Z, TetrisCell.Z},
+				new TetrisCell[3]{TetrisCell.J, TetrisCell.Z, 0           },
+				new TetrisCell[3]{TetrisCell.J, TetrisCell.J, TetrisCell.J},
+				new TetrisCell[3]{TetrisCell.T, TetrisCell.T, TetrisCell.T},
+				new TetrisCell[3]{0,            TetrisCell.T, 0           },
 			};
 
 			var expectedBoard = new TetrisCell[10][] {
@@ -63,11 +63,11 @@ namespace TetrisBackendTests
 				new TetrisCell[3]{0,            0,            0           },
 				new TetrisCell[3]{0,            0,            0           },
 				new TetrisCell[3]{0,            0,            0           },
-                new TetrisCell[3]{TetrisCell.Z, TetrisCell.Z, 0           },
-                new TetrisCell[3]{TetrisCell.L, 0,            TetrisCell.Z},
-                new TetrisCell[3]{0,            TetrisCell.Z, TetrisCell.Z},
-                new TetrisCell[3]{TetrisCell.J, TetrisCell.Z, 0           },
-                new TetrisCell[3]{0,            TetrisCell.T, 0           },
+				new TetrisCell[3]{TetrisCell.Z, TetrisCell.Z, 0           },
+				new TetrisCell[3]{TetrisCell.L, 0,            TetrisCell.Z},
+				new TetrisCell[3]{0,            TetrisCell.Z, TetrisCell.Z},
+				new TetrisCell[3]{TetrisCell.J, TetrisCell.Z, 0           },
+				new TetrisCell[3]{0,            TetrisCell.T, 0           },
 			};
 
 			var fullRows = board.FindFullLines();
@@ -76,6 +76,6 @@ namespace TetrisBackendTests
 			
 			board.ClearFullLines();
 			Assert.Equal(expectedBoard, board.Cells);
-        }
-    }
+		}
+	}
 }
--- tetris-backend/tetris-backend-tests/tetromino.cs
+++ tetris-backend/tetris-backend-tests/tetromino.cs
@@ -4,80 +4,80 @@ using TetrisBackend;
 
 namespace TetrisBackendTests 
 {
-    public class TetrominoTests
-    {
-        [Fact]
-        public void TetrominoInitialization()
-        {
-            // Transposed to [x, y] coordinates.
-            var tetromino_i_0 = new TetrisCell[][] {
-                new TetrisCell[]{0, 0, 0, 0},
-                new TetrisCell[]{TetrisCell.I, TetrisCell.I, TetrisCell.I, TetrisCell.I},
-                new TetrisCell[]{0, 0, 0, 0},
-                new TetrisCell[]{0, 0, 0, 0},
-            };
-            var tetromino_i_1 = new Tetromino(TetrisCell.I, new int[,] {
-                {0, 0, 0, 0},
-                {1, 1, 1, 1},
-                {0, 0, 0, 0},
-                {0, 0, 0, 0},
-            });
-            Assert.Equal(tetromino_i_0, tetromino_i_1.Cells);
-        }
+	public class TetrominoTests
+	{
+		[Fact]
+		public void TetrominoInitialization()
+		{
+			// Transposed to [x, y] coordinates.
+			var tetromino_i_0 = new TetrisCell[][] {
+				new TetrisCell[]{0, 0, 0, 0},
+				new TetrisCell[]{TetrisCell.I, TetrisCell.I, TetrisCell.I, TetrisCell.I},
+				new TetrisCell[]{0, 0, 0, 0},
+				new TetrisCell[]{0, 0, 0, 0},
+			};
+			var tetromino_i_1 = new Tetromino(TetrisCell.I, new int[,] {
+				{0, 0, 0, 0},
+				{1, 1, 1, 1},
+				{0, 0, 0, 0},
+				{0, 0, 0, 0},
+			});
+			Assert.Equal(tetromino_i_0, tetromino_i_1.Cells);
+		}
 
-        public static IEnumerable<object[]> rotationData = new Tetromino[][] {
-            new Tetromino[] {
-                new Tetromino(TetrisCell.Z, new int[,] {
-                    {1, 1, 0},
-                    {0, 1, 1},
-                    {0, 0, 0},
-                }),
-                new Tetromino(TetrisCell.Z, new int[,] {
-                    {0, 1, 0},
-                    {1, 1, 0},
-                    {1, 0, 0},
-                }),
-                new Tetromino(TetrisCell.Z, new int[,] {
-                    {0, 0, 1},
-                    {0, 1, 1},
-                    {0, 1, 0},
-                })
-            },
-            new Tetromino[] {
-                new Tetromino(TetrisCell.I, new int[,] {
-                    {0, 0, 0, 0},
-                    {1, 1, 1, 1},
-                    {0, 0, 0, 0},
-                    {0, 0, 0, 0},
-                }),
-                new Tetromino(TetrisCell.I, new int[,] {
-                    {0, 1, 0, 0},
-                    {0, 1, 0, 0},
-                    {0, 1, 0, 0},
-                    {0, 1, 0, 0},
-                }),
-                new Tetromino(TetrisCell.I, new int[,] {
-                    {0, 0, 1, 0},
-                    {0, 0, 1, 0},
-                    {0, 0, 1, 0},
-                    {0, 0, 1, 0},
-                })
-            }
-        };
+		public static IEnumerable<object[]> rotationData = new Tetromino[][] {
+			new Tetromino[] {
+				new Tetromino(TetrisCell.Z, new int[,] {
+					{1, 1, 0},
+					{0, 1, 1},
+					{0, 0, 0},
+				}),
+				new Tetromino(TetrisCell.Z, new int[,] {
+					{0, 1, 0},
+					{1, 1, 0},
+					{1, 0, 0},
+				}),
+				new Tetromino(TetrisCell.Z, new int[,] {
+					{0, 0, 1},
+					{0, 1, 1},
+					{0, 1, 0},
+				})
+			},
+			new Tetromino[] {
+				new Tetromino(TetrisCell.I, new int[,] {
+					{0, 0, 0, 0},
+					{1, 1, 1, 1},
+					{0, 0, 0, 0},
+					{0, 0, 0, 0},
+				}),
+				new Tetromino(TetrisCell.I, new int[,] {
+					{0, 1, 0, 0},
+					{0, 1, 0, 0},
+					{0, 1, 0, 0},
+					{0, 1, 0, 0},
+				}),
+				new Tetromino(TetrisCell.I, new int[,] {
+					{0, 0, 1, 0},
+					{0, 0, 1, 0},
+					{0, 0, 1, 0},
+					{0, 0, 1, 0},
+				})
+			}
+		};
 
-        [Theory]
-        [MemberData(nameof(rotationData))]
-        public void TetrominoRotation(Tetromino original, Tetromino leftExpected, Tetromino rightExpected)
-        {
-            var leftActual = new Tetromino(original);
-            leftActual.Rotate(Direction1D.Left);
-            
-            Assert.Equal(leftExpected.Cells, leftActual.Cells);
+		[Theory]
+		[MemberData(nameof(rotationData))]
+		public void TetrominoRotation(Tetromino original, Tetromino leftExpected, Tetromino rightExpected)
+		{
+			var leftActual = new Tetromino(original);
+			leftActual.Rotate(Direction1D.Left);
+			
+			Assert.Equal(leftExpected.Cells, leftActual.Cells);
 
-            var rightActual = new Tetromino(original);
-            rightActual.Rotate(Direction1D.Right);
+			var rightActual = new Tetromino(original);
+			rightActual.Rotate(Direction1D.Right);
 
-            Assert.Equal(rightExpected.Cells, rightActual.Cells);
-        }
-    }
+			Assert.Equal(rightExpected.Cells, rightActual.Cells);
+		}
+	}
 }
